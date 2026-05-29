8cd36f8336d60b8de4057f4c8aeaa370e0b152eb
TardigradeEnthusiast <leonyap.awesomeguy@gmail.com>
2026-05-29 17:58:10 -0500

    Second Commit
    Added monster mash, testing background functionality

---
+++ assets/js/CustomScripts.js
@@ -0,0 +1,5 @@
+function BgChange() {
+    // document.getElementById("bg").classList.toggle("MonsterBg") = "url('../../images/mmash shoppage newnoscale.png')" ;
+    document.body.classList.toggle("MonsterBg") ;
+    print("bg try");
+}
